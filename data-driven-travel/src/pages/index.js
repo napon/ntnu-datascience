95a904082f8d82822341536f34c915443d77cc2e
Napon Taratan <napon@napontaratan.com>
2023-10-28 18:07:16 +0800

    Add hover ability on provinces

--- data-driven-travel/src/pages/index.js
+++ data-driven-travel/src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Particles from "react-particles";
 import { loadLinksPreset } from "tsparticles-preset-links";
-import { 
+import {
   GeoJSON,
   MapContainer,
   Marker,
@@ -52,6 +52,7 @@ const clip2 = {
 const MapView = ({setShowMap}) => {
   const [showDropdown, setShowDropdown] = React.useState(false)
   const [selectedMonth, setSelectedMonth] = React.useState((new Date()).getMonth())
+  const [hoverProvince, setHoverProvince] = React.useState(null)
 
   const mapData = React.useMemo(() => {
     const output = {...DEFAULT_MAP_GEOJSON}
@@ -71,46 +72,57 @@ const MapView = ({setShowMap}) => {
     return {...best.properties}
   }, [mapData])
 
+  const provinceLocationMap = React.useRef({});
+
   const highlightFeature = (e => {
     let layer = e.target;
     const feature = e.target.feature.properties;
     layer.setStyle({
       fillOpacity: 1
     })
-  })
-  
-  const resetHighlight= (e =>{
-    e.target.setStyle(style(e.target.feature));
+    setHoverProvince(feature)
   })
 
-  const onEachFeature= (feature, layer) => {
+  const resetHighlight = (e =>{
+    e.target.setStyle(style(e.target.feature));
+    setHoverProvince(null)
+  })
+
+  const onEachFeature = (feature, layer) => {
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight
     });
+    provinceLocationMap.current[feature.properties.CHA_NE] = layer.getBounds().getCenter()
   }
 
   if (typeof window !== 'undefined') {
     const selectedProvinceName = bestProvince?.CHA_NE?.replace(/([A-Z])/g, ' $1').trim()
-
+    const selectedProvince = bestProvince.CHA_NE
     return (
       <>
-        <MapContainer id="map" style={{ height: "100vh" }} center={DEFAULT_COORDS} zoom={6} scrollWheelZoom={false}>
+        <MapContainer
+          id="map"
+          style={{ height: "100vh" }}
+          center={DEFAULT_COORDS}
+          zoom={6}
+          scrollWheelZoom={false}
+        >
           <div style={{zIndex: 999}} className="absolute top-4 right-4 ">
             <div className="flex content-end mb-4 place-content-end">
               <div onMouseOver={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
-                <button 
-                  id="dropdownDefaultButton" 
+                <button
+                  id="dropdownDefaultButton"
                   className="text-sky-600 bg-white border-slate-400 shadow-md focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-4" type="button">{DISPLAY_MONTHS[selectedMonth]}
                     <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"
                     />
                 </svg>
                 </button>
-  
+
                 <div style={{zIndex: 999}} className={`z-10 shadow-lg place-content-start ${showDropdown ? "absolute" : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 top-10`}>
                     <ul className="py-2 text-sm text-sky-600" aria-labelledby="dropdownDefaultButton">
-                      { DISPLAY_MONTHS.map((month, idx) => 
+                      { DISPLAY_MONTHS.map((month, idx) =>
                         <li key={month} onClick={(e) => {setSelectedMonth(idx); setShowDropdown(false)}}>
                           <a className="block px-4 py-2 hover:bg-gray-100">{month}</a>
                         </li>)
@@ -118,10 +130,10 @@ const MapView = ({setShowMap}) => {
                     </ul>
                 </div>
               </div>
-              <a 
-                href="" 
+              <a
+                href=""
                 style={{color: "white"}}
-                onClick={(e) => { e.preventDefault(); setShowMap(false); setShowDropdown(false);}} 
+                onClick={(e) => { e.preventDefault(); setShowMap(false); setShowDropdown(false);}}
                 className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
               >
                 Exit
@@ -130,23 +142,34 @@ const MapView = ({setShowMap}) => {
             <div className="bg-slate-100 leaflet-control leaflet-bar p-4 shadow-md max-w-md text-sm">
               <strong>Discover the best places to visit</strong>
               <p className="mb-8">Select the desired month from the dropdown above, or hover on each province for more details.</p>
-              <>
-                <p className="mb-1">The best province to visit in <b>{DISPLAY_MONTHS[selectedMonth]}</b> is {selectedProvinceName}.</p>
-                <p>Rainfall: {Math.round(bestProvince.max_rainfall_mm, 2)}mm</p>
-                <p>Tourism Ratio: {Math.round(bestProvince.tourism_ratio, 2)}%</p>
-                <p>Overall Score: {Math.round(bestProvince.score * 100, 2)}%</p>
-              </>
+              {
+                hoverProvince ?
+                  <>
+                    <p className="mb-1">Data for <b>{hoverProvince.CHA_NE?.replace(/([A-Z])/g, ' $1').trim()}</b>:</p>
+                    <p>Rainfall: {Math.round(hoverProvince.max_rainfall_mm, 2)}mm</p>
+                    <p>Tourism Ratio: {Math.round(hoverProvince.tourism_ratio, 2)}%</p>
+                    <p>Overall Score: {Math.round(hoverProvince.score * 100, 2)}%</p>
+                  </>
+                :
+                  <>
+                    <p className="mb-1">The best province to visit in <b>{DISPLAY_MONTHS[selectedMonth]}</b> is {selectedProvinceName}.</p>
+                    <p>Rainfall: {Math.round(bestProvince.max_rainfall_mm, 2)}mm</p>
+                    <p>Tourism Ratio: {Math.round(bestProvince.tourism_ratio, 2)}%</p>
+                    <p>Overall Score: {Math.round(bestProvince.score * 100, 2)}%</p>
+                  </>
+              }
             </div>
           </div>
-          
+
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <GeoJSON data={mapData} 
+          <GeoJSON data={mapData}
             onEachFeature={onEachFeature}
             style={style}
           />
+          {provinceLocationMap.current[selectedProvince] && <Marker position={provinceLocationMap.current[selectedProvince]}/>}
         </MapContainer>
       </>
     )
@@ -156,7 +179,7 @@ const MapView = ({setShowMap}) => {
 const IndexPage = () => {
 
   const [showMap, setShowMap] = React.useState(false)
-  
+
   const wrapperStyle = ['relative', 'isolate']
   if (!showMap) {
     wrapperStyle.concat(['px-6','lg:px-8'])
@@ -165,7 +188,7 @@ const IndexPage = () => {
   return (
     <main>
       <div className="bg-white">
-        <Particles 
+        <Particles
           options={{
             preset: "links",
             background: {
@@ -176,15 +199,15 @@ const IndexPage = () => {
                 color: "#4878CD"
               }
             },
-            
-          }} 
+
+          }}
           init={async(engine) => loadLinksPreset(engine)}
         />
         <div className={wrapperStyle.join(' ')}>
           {
             showMap && <MapView setShowMap={setShowMap} />
           }
-          { !showMap && 
+          { !showMap &&
             <div>
               <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
                 <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#42a7f5] to-[#42e6f5] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={clip1}></div>
@@ -204,7 +227,7 @@ const IndexPage = () => {
               </div>
             </div>
           }
-          
+
         </div>
       </div>
     </main>
@@ -215,7 +238,7 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => 
+export const Head = () =>
   <>
     <title>Data Driven Travel</title>
   </>
